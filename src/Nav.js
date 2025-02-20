aa882b99340aa6e17ba8f25def04bb8590707634
joyegwuatu <egwuatujoy60@gmail.com>
2025-02-20 14:31:13 +0100

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <h1>TODO-APP</h1>
+      <button>my todos</button>
+    </div>
+  );
+};
+
+export default Nav;
